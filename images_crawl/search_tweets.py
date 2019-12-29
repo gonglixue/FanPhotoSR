1f6b2db2ab911eb2bc9bae5eec39cd931fc961ac
gonglixue <724351282@qq.com>
2019-12-29 18:23:03 +0800

    photo-container

--- images_crawl/search_tweets.py
+++ images_crawl/search_tweets.py
@@ -12,13 +12,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+from urllib import request
+
 # https://twitter.com/search?q=191222%20since%3A2019-01-01%20until%3A2019-01-02&src=typd
 # https://twitter.com/search?q=191222%20until%3A2019-01-02%20since%3A2019-01-01&src=typed_query
 def init_driver(driver_type):
     if driver_type == 1:
         driver = webdriver.Firefox()
     elif driver_type == 2:
-        driver = webdriver.Chrome("./chromedriver.exe")
+        driver = webdriver.Chrome("F:\PycharmProject\FanPhotoSR\images_crawl\chromedriver.exe")
     elif driver_type == 3:
         driver = webdriver.Ie()
     elif driver_type == 4:
@@ -29,22 +31,25 @@ def init_driver(driver_type):
     return driver
 
 
-def scroll(driver, start_date, end_date, words, lang, max_time=180):
+def scroll(driver, start_date, end_date, words, lang, max_time=20, account=None):
     languages = { 1: 'en', 2: 'it', 3: 'es', 4: 'fr', 5: 'de', 6: 'ru', 7: 'zh'}
     url = "https://twitter.com/search?q="
     for w in words[:-1]:
         url += "{}%20OR".format(w)
-    url += "{}%20".format(words[-1])
+    url += "{}".format(words[-1])
+    if account is not None:
+        url += "(from%3A{})".format(account)
+    url += "%20"
     url += "until%3A{}%20since%3A{}&".format(end_date, start_date)
     if lang != 0:
         url += "l={}&".format(languages[lang])
     url += "src=typed_query"
-    print(url)
+    print("url", url)
     driver.get(url)
     start_time = time.time()  # remember when we started
     while (time.time() - start_time) < max_time:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        print( str(time.time() - start_time) + " < " + str(max_time) )
+        print( str(time.time() - start_time) + " < " + str(max_time) , end='\r')
 
 
 def scrape_tweets(driver):
@@ -68,15 +73,35 @@ def scrape_tweets(driver):
             date = (c.find_all("span", class_="_timestamp")[0].string).strip()
 
             datestring = str(c.find_all("span", class_="_timestamp")[0])
-            print(datestring)
             datestring = datestring[datestring.index("data-time")+11:]
             datestring = datestring[:datestring.index("\"")]
             print(datestring)
-            # print(tweet_text)            
-            try:
-                write_csv(datestring,tweet_text,name)
-            except:
-                print('csv error')
+            print(tweet_text)      
+
+            photo_container = c.find_all("div", class_="AdaptiveMedia-photoContainer")      
+            if len(photo_container) > 0:
+                print("find {} photo container".format(len(photo_container)))
+                for pc in photo_container:
+                    img_tags = pc.find_all("img")[0]
+                    src = img_tags["src"]
+                    img_name = src.split('/')[-1]
+
+                    # request image url
+                    print("img url:", src+":large")
+                    req = request.Request(src+":large", headers={
+                       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0'
+                    })
+                    data = request.urlopen(req, timeout=30).read()
+                    f = open(img_name, 'wb')
+                    f.write(data)
+                    f.close()
+                    # request by driver
+            else:
+                print("no photo")
+            # try:
+            #     write_csv(datestring,tweet_text,name)
+            # except:
+            #     print('csv error')
 
     except Exception as e:
         print("Something went wrong!")
@@ -126,24 +151,30 @@ def get_all_dates(start_date, end_date):
 
 
 def main():
-    driver_type = int(input("1) Firefox | 2) Chrome | 3) IE | 4) Opera | 5) PhantomJS\nEnter the driver you want to use: "))
-    wordsToSearch = input("Enter the words: ").split(',')
-    for w in wordsToSearch:
-        w = w.strip()
-    start_date = input("Enter the start date in (YYYY-MM-DD): ")
-    end_date = input("Enter the end date in (YYYY-MM-DD): ")
+    # driver_type = int(input("1) Firefox | 2) Chrome | 3) IE | 4) Opera | 5) PhantomJS\nEnter the driver you want to use: "))
+    # wordsToSearch = input("Enter the words: ").split(',')
+    # for w in wordsToSearch:
+    #     w = w.strip()
+    # start_date = input("Enter the start date in (YYYY-MM-DD): ")
+    # end_date = input("Enter the end date in (YYYY-MM-DD): ")
+    # account = input("Enter account(without @): ")
+    # lang = int(input("0) All Languages 1) English | 2) Italian | 3) Spanish | 4) French | 5) German | 6) Russian | 7) Chinese\nEnter the language you want to use: "))
 
-    lang = int(input("0) All Languages 1) English | 2) Italian | 3) Spanish | 4) French | 5) German | 6) Russian | 7) Chinese\nEnter the language you want to use: "))
-    all_dates = get_all_dates(start_date, end_date)
-    print(all_dates)
+    driver_type = 2
+    wordsToSearch = ["191222"]
+    start_date = "2019-12-21"
+    end_date = "2019-12-23"
+    account = "yeoreum110"
+    lang = 0
+
     write_csv_header()
-    for i in range(len(all_dates) - 1):
-        driver = init_driver(driver_type)
-        scroll(driver, str(all_dates[i]), str(all_dates[i + 1]), wordsToSearch, lang)
-        scrape_tweets(driver)
-        time.sleep(5)
-        print("The tweets for {} are ready!".format(all_dates[i]))
-        driver.quit()
+
+    driver = init_driver(driver_type)
+    scroll(driver, start_date, end_date, wordsToSearch, lang, account=account)
+    scrape_tweets(driver)
+    time.sleep(5)
+    print("finish!")
+    driver.quit()
 
 
 if __name__ == "__main__":

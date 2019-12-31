26631f500e871ab5caa4ae002293458185a20f20
gonglixue <724351282@qq.com>
2020-01-01 01:56:11 +0800

    resume-v0

--- images_crawl/DriverManager/driver_manager.py
+++ images_crawl/DriverManager/driver_manager.py
@@ -2,6 +2,7 @@ import datetime
 import time
 from dateutil.relativedelta import relativedelta
 import os
+import re
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -14,9 +15,11 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+
+
 class DriverManager(object):
 
-    def __init__(self, config, log_file):
+    def __init__(self, config, log_file, status_log):
         self.account = config["account"]
         self.start_time = config["start_time"]
         self.end_time = config["end_time"]
@@ -24,7 +27,8 @@ class DriverManager(object):
         self.save_root = config["save_root"]
         self.timeout = config["timeout"] # s
         self.log_file = log_file
-        self.scroll_try_max = 5
+        self.status_log = status_log
+        self.scroll_try_max = 3
 
     @staticmethod
     def construct_url(account, start_time, end_time):
@@ -35,9 +39,12 @@ class DriverManager(object):
         )
         return url
 
-    def crawl(self):
+    def crawl(self, resume_date=None):
         # get all date range by month
-        all_dates = self.__get_all_months__()
+        if resume_date is None:
+            all_dates = self.__get_all_months__()
+        else:
+            all_dates = self.__get_all_months__(resume_date)
         total_counts = 0
 
         for i in range(len(all_dates)-1):
@@ -52,7 +59,9 @@ class DriverManager(object):
             total_counts += query_images_count
 
             print("------------------- {}".format(query_images_count))
-            self.log_file.write("   {} - {}, {}\n".format(query_start, query_end,query_images_count))
+            self.log_file.flush()
+            self.status_log.write("{}, {}\n".format(self.account, query_end))
+            self.status_log.flush()
 
         print("\n\n\n Finish")
         print(total_counts)
@@ -74,35 +83,51 @@ class DriverManager(object):
             print("------{} tweets are found".format(len(content)))
             # self.log_file.write("   {} tweets found\n".format(len(content)))
             for c in content:
+                photo_container = c.find_all("div", class_="AdaptiveMedia-photoContainer")   
+                if len(photo_container) == 0:
+                    print("------------no photo found")
+                    continue
+                # datestring in tweets-text
+                tweets = c.find("p", class_="tweet-text").strings
+                tweets_text = "".join(tweets)
+                photo_date = re.sub("\D", "", tweets_text)[0:6] # eg. 191231
+
+                # date of posint tweets
                 datestring = str(c.find_all("span", class_="_timestamp")[0])
                 datestring = datestring[datestring.index("data-time")+11:]
                 datestring = datestring[:datestring.index("\"")]
                 tweet_time = time.localtime(int(datestring))
-                month = "%02d"%(int(tweet_time.tm_mon))
-                day = "%02d"%(int(tweet_time.tm_mday))
+                tweet_year = "%4d"%(int(tweet_time.tm_year))
+                tweet_month = "%02d"%(int(tweet_time.tm_mon))
+                tweet_day = "%02d"%(int(tweet_time.tm_mday))
 
-                photo_container = c.find_all("div", class_="AdaptiveMedia-photoContainer")      
-                if len(photo_container) > 0:
-                    print("------------find {} photo container".format(len(photo_container)))
-                    images_count += len(photo_container)
+                if len(photo_date) != 6:
+                    photo_date = "000000"
+                photo_month = photo_date[2:4]
+                photo_day = photo_date[4:-1]
 
-                    for pc in photo_container:
-                        img_tags = pc.find_all("img")[0]
-                        src = img_tags["src"]
-                        img_name = src.split('/')[-1]
+                if int(photo_month) < 1 or int(photo_month) > 12:
+                    photo_month = "00"
+                    print("------------parse photo date falied:", photo_date)
+                
+                print("------------find {} photo container".format(len(photo_container)))
+                self.log_file.write("{}, {}-{}-{}, {}\n".format(self.account, tweet_year, tweet_month, tweet_day, len(photo_container)))
+                images_count += len(photo_container)
 
-                        # request image url
-                        print("------------img url:", src+":large")
-                        response = requests.get(src+":large")
-                        image = Image.open(BytesIO(response.content))
-                        
-                        img_name = "{}-{}-{}-{}".format(month, day, self.account, img_name)
-                        image.save(os.path.join(self.save_root, month, img_name))
-                        print("------------save image:", img_name)
-                        
-                else:
-                    print("------------no photo")
-                    images_count += 0
+                for pc in photo_container:
+                    img_tags = pc.find_all("img")[0]
+                    src = img_tags["src"]
+                    img_name = src.split('/')[-1]
+
+                    # request image url
+                    print("------------img url:", src+":large")
+                    response = requests.get(src+":large")
+                    image = Image.open(BytesIO(response.content))
+                    
+                    img_name = "{}-{}-{}".format(photo_date, self.account, img_name)
+                    image.save(os.path.join(self.save_root, photo_month, img_name))
+                    print("------------save image:", img_name)
+
         except Exception as e:
             print("Something woring in __parse_page__")
             print(e)
@@ -117,11 +142,6 @@ class DriverManager(object):
         # scroll until load all data
         while (time.time() - start_time) < self.timeout:
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            # time.sleep(3)
-            # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight-1000);")
-            # time.sleep(2)
-            # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            # time.sleep(5)
 
             current_scroll_height = self.driver.execute_script("return document.body.scrollHeight;")
             if current_scroll_height == last_scorll_height:
@@ -143,9 +163,12 @@ class DriverManager(object):
 
 
 
-    def __get_all_months__(self):
+    def __get_all_months__(self, resume_date=None):
         all_dates = []
-        start_date = datetime.datetime.strptime(self.start_time, "%Y-%m-%d")
+        if resume_date is None:
+            start_date = datetime.datetime.strptime(self.start_time, "%Y-%m-%d")
+        else:
+            start_date = datetime.datetime.strptime(resume_date, "%Y-%m-%d")
         end_date = datetime.datetime.strptime(self.end_time, "%Y-%m-%d")
         # step = relativedelta(months=+1)
         step = datetime.timedelta(days=15)
--- images_crawl/search_tweets_v2.py
+++ images_crawl/search_tweets_v2.py
@@ -1,4 +1,5 @@
 import os
+import time
 from DriverManager import TwitterImageCrawler
 
 f = open("twitter_address.txt", "r")
@@ -9,26 +10,69 @@ config = {
     "account":None,
     "start_time": "2019-01-01",
     "end_time": "2019-12-31",
-    "save_root": "2019photo",
+    "save_root": "2019photo2",
     "timeout": 600
 }
 
+def resume_status(status_log):
+    line = ""
+    for ll in status_log:
+        line = ll.strip()
+    if len(line) == 0 or line is None:
+        return "", ""
+    account, last_date = line.split(",")
+    account = account.strip()
+    last_date = last_date.strip()
+    return account, last_date
+    
 
 if __name__ == "__main__":
+    mode = "scratch" # "resume"
+    resume_account, resume_date = "", ""
+
     # init folder
-    log = open("crawl_log.txt", "w")
+    if not os.path.exists("crawl_log.txt") or mode=="scratch":
+        log = open("crawl_log.txt", "w")
+    else:
+        log = open("crawl_log_{}.txt".format(time.strftime("%Y%m%d-%H%M%S", time.localtime()) ), "w")
+    if mode == "scratch":
+        status_log = open("status_log.txt", "w")
+    else:
+        status_log = open("status_log.txt", "r")
+        resume_account, resume_date = resume_status(status_log)
+        status_log.close()
+        status_log = open("status_log.txt", "a")
+
     if not os.path.exists(config["save_root"]):
         os.mkdir(config["save_root"])
-    for i in range(1, 13):
+    for i in range(0, 13):
         sub_folder = os.path.join(config["save_root"], "%02d"%i)
         if not os.path.exists(sub_folder):
             os.mkdir(sub_folder)
 
+    begin_ind = 0
+    if resume_account != "":
+        for acc in all_account:
+            if acc == resume_account:
+                break
+            begin_ind += 1
+    all_account = all_account[begin_ind: -1]
+    print("search {} accounts: ".format(len(all_account)))
+    print(all_account)
+
+    resume_flag = True
     for account in all_account:
         config["account"] = account
-        crawler = TwitterImageCrawler(config, log)
-        num = crawler.crawl()
+        crawler = TwitterImageCrawler(config, log, status_log)
+        if resume_flag:
+            if resume_date == "":
+                resume_date = None
+            num = crawler.crawl(resume_date)
+            resume_flag = False
+        else:
+            num = crawler.crawl()
         print("finish account {} with {} images".format(account, num))
         # log.write("{} {}".format(account, num))
 
     log.close()
+    status_log.close()
